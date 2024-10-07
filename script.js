3cc0b7c9757ccf4be3bacd1d4317f3c650bedaf2
KNXCSdev <michal.miksho@gmail.com>
2024-10-07 13:32:26 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+"use strict";
+//SECTION Constructor functions
+//NOTE CONSTRUCTOR FUNCTIONS ALWAYS START WITH A UPPERCASE LETER
+const Person = function (firstName, birthYear) {
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person("Jonas", 1991);
+
+//1. New {} is created
+//2. funtion is called, this={}
+//3. {} linked to prototype
+//4. funtion automatically returns {}
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 2017);
+
+console.log(jonas, matilda, jack);
+
+const jay = "jay";
+
+console.log(jack instanceof Person);
+console.log(jay instanceof Person);
+
+//SECTION PROTOTYPES
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+Person.prototype.species = "Homo Sappiens";
+console.log(jonas.hasOwnProperty("species"));
+
+//SECTION Prototypal Inheritance on Built-In Objects
+
+console.log(jonas.__proto__.__proto__);
+
+const arr = [3, 6, 4, 5, 6, 9, 3, 3, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__ == Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir(h1);
+
+//CHALLANGE 1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+const car1 = new Car("BMW", 120);
+const car2 = new Car("Mercedes", 150);
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+
+car2.accelerate();
+car2.brake();
